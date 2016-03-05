c39267403cc7a01887c5cd5e46e1cf061446adb6
henrikrudstrom <hrudstrom@gmail.com>
2016-03-05 16:51:16 +0100

    only render files that need screenshot

--- render.js
+++ render.js
@@ -1,22 +1,29 @@
 /**
- * Created by henrik on 29.02.16.
+ * Created by henrikrudstrom on 29.02.16.
  */
-var exec = require("child_process").execFileSync
-var glob = require("glob")
+var fs = require('fs');
+var exec = require("child_process").execFileSync;
+var glob = require("glob");
 
-var devPath = __dirname.split("/").pop()
+var devPath = __dirname.split("/").pop();
 
 var paths = [
   "tests/Render/Simple/*.qml"
-]
+];
 
-paths.forEach(function(path){
-  glob(path, function (er, files) {
-    files.forEach(render)
-  })
-})
+paths.forEach(function(path) {
+  glob(path, function(er, files) {
+    files.forEach(render);
+  });
+});
 
-function render(path){
-  console.log(path)
-  exec(devPath + "/bin/shorty", [devPath + "/simple_screenshot.qml", path])
+function render(path) {
+  console.log(path);
+  var png = path.replace(".qml", ".png");
+  try {
+    fs.accessSync(png, fs.F_OK);
+  } catch (e) {
+    exec(devPath + "/bin/shorty", [devPath + "/simple_screenshot.qml", path]);
+  }
+
 }
