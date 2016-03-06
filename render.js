5cb26400c0f8d2ff78790b0ac3fdb390d0394270
henrikrudstrom <hrudstrom@gmail.com>
2016-03-06 20:03:07 +0100

    support //@Render flag in qml test files, will render .png if it does not exist

--- render.js
+++ render.js
@@ -6,16 +6,34 @@ var exec = require("child_process").execFileSync;
 var glob = require("glob");
 
 var devPath = __dirname.split("/").pop();
-
 var paths = [
   "tests/Render/Simple/*.qml",
   "tests/QtQuick/qml/TextBasic.qml",
   "tests/QtQuick/qml/RectangleColor.qml"
 ];
 
+function isRenderTest(file, callback) {
+  var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(file)
+  });
+
+  lineReader.on('line', function(line) {
+    if (line.indexOf("@Render") != -1) {
+      callback();
+    }
+    lineReader.close();
+  });
+
+}
+
 paths.forEach(function(path) {
   glob(path, function(er, files) {
     files.forEach(render);
+  });
+  glob("tests/**/*.qml", function(er, files) {
+    files.forEach(function(file) {
+      isRenderTest(file, function() { render(file); });
+    });
   });
 });
 
